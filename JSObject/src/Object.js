d74b718359939a7659a98118b3157d22679d2c78
pavel <pamix1985@gmail.com>
2019-07-14 19:25:06 +0700

    Задача Работа с объектами исправление замечаний.

--- JSObject/src/Object.js
+++ JSObject/src/Object.js
@@ -1,43 +1,27 @@
-var countries = [norway = {}, usa = {}, russia = {}];
-norway.name = "Norway";
-usa.name = "USA";
-russia.name = "Russia";
-
-norway.cities = [oslo = {}, bergen = {}, trondheim = {}, stavanger = {}];
-oslo.name = "Oslo";
-oslo.population = 1026758;
-
-bergen.name = "Bergen";
-bergen.population = 271949;
-
-trondheim.name = "Trondheim";
-trondheim.population = 182035;
-
-stavanger.name = "Stavanger";
-stavanger.population = 130754;
-
-usa.cities = [wahshington = {}, newYork = {}, losAngeles = {}];
-wahshington.name = "Washington";
-wahshington.population = 633427;
-
-newYork.name = "NewYork";
-newYork.population = 8623000;
-
-losAngeles.name = "LosAngeles";
-losAngeles.population = 4000000;
-
-russia.cities = [novosibirsk = {}, moscow = {}, saintPetersburg = {}, krasnoyarsk = {}];
-novosibirsk.name = "Novosibirsk";
-novosibirsk.population = 1511000;
-
-moscow.name = "Moscow";
-moscow.population = 11920000;
-
-saintPetersburg.name = "SaintPetersburg";
-saintPetersburg.population = 4991000;
-
-krasnoyarsk.name = "Krasnoyarsk";
-krasnoyarsk.population = 1007000;
+var countries = [
+    norway = {
+        name: "Norway", cities: [
+            oslo = {name: "Oslo", population: 1026758},
+            bergen = {name: "Bergen", population: 271949},
+            trondheim = {name: "Trondheim", population: 182035},
+            stavanger = {name: "Stavanger", population: 130754}
+        ]
+    },
+    usa = {
+        name: "USA", cities: [
+            washington = {name: "Washington", population: 633427},
+            newYork = {name: "NewYork", population: 8623000},
+            losAngeles = {name: "LosAngeles", population: 4000000}
+        ]
+    },
+    russia = {
+        name: "Russia", cities: [
+            novosibirsk = {name: "Novosibirsk", population: 1511000},
+            moscow = {name: "Moscow", population: 11920000},
+            saintPetersburg = {name: "saintPetersburg", population: 4991000},
+            krasnoyarsk = {name: "Krasnoyarsk", population: 1007000}
+        ]
+    }];
 
 var countriesWithMaxCitiesList = getCountriesListWithMaxCities(countries);
 console.log(countriesWithMaxCitiesList);
@@ -45,13 +29,10 @@ console.log(countriesWithMaxCitiesList);
 var informationObject = getCountriesPopulationList(countries);
 console.log(informationObject);
 
-function getCityPopulation(city) {
-    return city.population;
-}
-
 function getSumPopulation(country) {
-    return country.cities.map(getCityPopulation).reduce(function (sum, number) {
-        return sum + number;
+    return country.cities.reduce(function (sum, city) {
+        var currentPopulation = city.population;
+        return sum + currentPopulation;
     }, 0);
 }
 
@@ -64,25 +45,20 @@ function getCountriesPopulationList(countries) {
     return obj;
 }
 
-function getCitiesCount(country) {
-    return country.cities.length;
-}
-
-function sortDescending(a, b) {
-    return b - a;
-}
-
-function getName(country) {
-    return country.name;
-}
-
 function createFilteredList(countries) {
+    var maxCitiesValue = countries.map(function (country) {
+        return country.cities.length;
+    }).sort(function (a, b) {
+        return b - a;
+    })[0];
     return countries.filter(function (item) {
-        return item.cities.length === countries.map(getCitiesCount).sort(sortDescending)[0];
+        return item.cities.length === maxCitiesValue;
     });
 }
 
 function getCountriesListWithMaxCities(countries) {
-    return createFilteredList(countries).map(getName);
+    return createFilteredList(countries).map(function (country) {
+        return country.name;
+    });
 }
 
