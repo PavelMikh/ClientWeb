eacc922e498ba9da1e488aa1c7013ea3a36ff23d
pavel <pamix1985@gmail.com>
2019-07-25 22:12:33 +0700

    Задача TODO List дописал стили.

--- TodoList/src/JS/TodoListScript.js
+++ TodoList/src/JS/TodoListScript.js
@@ -1,57 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var newTodoList = document.getElementById("todo-list");
+    var newTodoList = document.getElementById("new-todo-list");
     var newTodoText = document.getElementById("new-todo-text");
     newTodoText.focus();
     var validationMessage = document.querySelector(".validation-message");
     var addTodoButton = document.getElementById("add-new-todo");
 
-    function createLi(text) {
+    function createNote(text) {
         var li = document.createElement("li");
         li.innerHTML = "<span class='text'></span><button type='button'>x</button>" +
-            "<button type='button'>edit</button>";
+            "<button type='button'>редактировать</button>";
         li.children[0].textContent = text;
         return li;
     }
 
     addTodoButton.addEventListener("click", function () {
-        var newText = newTodoText.value;
-        if (newText === "") {
+        var newNote = newTodoText.value;
+        if (newNote === "") {
             validationMessage.style.display = "block";
+            newTodoText.focus();
             return;
         }
 
+        newTodoList.style.display = "block";
+
         validationMessage.style.display = "none";
 
-        var li = createLi(newText);
+        var li = createNote(newNote);
         li.children[1].addEventListener("click", function () {
             newTodoList.removeChild(li);
+            if (newTodoList.getElementsByTagName("li").length === 0) {
+                newTodoList.style.display = "none";
+            }
+            newTodoText.focus();
         });
 
         li.children[2].addEventListener("click", function () {
-            var div = document.createElement("div");
-            var editInput = document.createElement("input");
-            editInput.id = "edit_input";
-            editInput.type = "text";
-            editInput.value = li.children[0].innerText;
-            div.appendChild(editInput);
+            var editingPanel = document.createElement("div");
+            var newValueInput = document.createElement("input");
+            newValueInput.type = "text";
+            newValueInput.value = li.children[0].innerText;
+            editingPanel.appendChild(newValueInput);
             var saveButton = document.createElement("button");
             saveButton.type = "button";
-            saveButton.innerHTML = "save";
-            div.appendChild(saveButton);
-            var deleteButton = document.createElement("button");
-            deleteButton.type = "button";
-            deleteButton.innerHTML = "cancel";
-            div.appendChild(deleteButton);
-            newTodoList.replaceChild(div, li);
+            saveButton.innerHTML = "сохранить";
+            editingPanel.appendChild(saveButton);
+            var cancelButton = document.createElement("button");
+            cancelButton.type = "button";
+            cancelButton.innerHTML = "отменить";
+            editingPanel.appendChild(cancelButton);
+            newTodoList.replaceChild(editingPanel, li);
+            newValueInput.focus();
             saveButton.addEventListener("click", function () {
-                li.children[0].textContent = editInput.value;
-                newTodoList.replaceChild(li, div);
+                li.children[0].textContent = newValueInput.value;
+                newTodoList.replaceChild(li, editingPanel);
+                newTodoText.focus();
             });
-            deleteButton.addEventListener("click", function () {
-                newTodoList.replaceChild(li, div);
+            cancelButton.addEventListener("click", function () {
+                newTodoList.replaceChild(li, editingPanel);
+                newTodoText.focus();
             });
         });
-
         newTodoList.appendChild(li);
         newTodoText.value = "";
         newTodoText.focus();
